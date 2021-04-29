5b4b7612adda0a3d95200eca3659e8ace7344ada
r <you@example.com>
2021-04-29 23:37:02 +0300

    my normal app

--- main.go
+++ main.go
@@ -1,11 +1,7 @@
 package main
 
 import (
-	"fmt"
-	_ "github.com/go-sql-driver/mysql"
-	"log"
-	"my.localhost/others/bookstore/config"
-	"my.localhost/others/bookstore/models/books"
+	"my.localhost/others/bookstore/handler"
 	"net/http"
 )
 
@@ -15,19 +11,6 @@ import (
 */
 
 func main() {
-	http.HandleFunc("/books", booksIndex)
+	http.HandleFunc("/books", handler.BooksIndex)
 	http.ListenAndServe(":3000", nil)
-}
-
-func booksIndex(w http.ResponseWriter, r *http.Request) {
-	bks, err := books.All(config.DB)
-	if err != nil {
-		log.Println(err)
-		http.Error(w, http.StatusText(500), 500)
-		return
-	}
-
-	for _, bk := range bks {
-		fmt.Fprintf(w, "%s, %s, %s, £%.2f\n", bk.Isbn, bk.Title, bk.Author, bk.Price)
-	}
 }
