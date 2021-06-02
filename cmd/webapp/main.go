763e8599ab9444031faa1f9286a0baa7384c0480
r <you@example.com>
2021-06-03 01:14:16 +0300

    fmt main.go

--- cmd/webapp/main.go
+++ cmd/webapp/main.go
@@ -1,13 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"my.localhost/others/bookstore/internal/handler"
 	"net/http"
-    "fmt"
 )
 
 func main() {
-    fmt.Println("Default route: GET /books")
+	fmt.Println("Default route: GET /books")
 	http.HandleFunc("/books", handler.BooksIndex)
 	http.ListenAndServe(":3000", nil)
 }
